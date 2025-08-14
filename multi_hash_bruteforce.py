38d9d3dac8f07b72c474a38c35df2ee086cafb5b
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-08-14 16:19:08 -0600

    multi_hash_bruteforce.py

--- multi_hash_bruteforce.py
+++ multi_hash_bruteforce.py
@@ -198,6 +198,18 @@ def validate_word(word, target_hash, hash_type, ssid, user):
         except binascii.Error:
           return pbkf_sha1_passlib.verify(word, target_hash)
 
+    elif hash_type == "pbkdf2-sha512":
+         try:
+
+           algo, iterations, salt_b64, key_b64 = target_hash.split('$')[1:]
+           dklen = len(b64decode(key_b64))
+           salt = b64decode(salt_b64)
+           key = pbkdf2_hmac('sha512', data, salt, int(iterations), dklen)
+           return f"$pbkdf2-sha512${iterations}${b64encode(salt).decode()}${b64encode(key).decode()}"
+
+         except binascii.Error:
+           return pbkf_sha5_passlib.verify(word, target_hash)
+
     elif hash_type in SUPPORTED_HASHES:
         if hash_type == 'sha512crypt':
             time.sleep(0.02)

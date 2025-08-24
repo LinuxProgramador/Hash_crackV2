8cd2683bb5f260dc75d9e8ae835ebf6046e246fa
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-08-24 08:07:43 -0600

    hash_validator.py

--- hashlibx/hash_validator.py
+++ hashlibx/hash_validator.py
@@ -185,6 +185,8 @@ def validate_word(word, data,  target_hash, hash_type, encoder, wpa_psk, ssid, u
               try:
                 hash_type_sche = hash_type.replace("sha512crypt","sha512_crypt")
                 hash_type_sche = hash_type_sche.replace("sha256crypt","sha256_crypt")
+                hash_type_sche = hash_type_sche.replace("md5crypt","md5_crypt")
+                hash_type_sche = hash_type_sche.replace("apr1","apr_md5_crypt")
                 context = CryptContext(schemes=[hash_type_sche])
                 return context.verify(word, target_hash)
 

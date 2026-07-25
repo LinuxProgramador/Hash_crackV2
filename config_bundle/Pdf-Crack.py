3f197c50b300b9dcecfba91305999409f61f5baa
JP Rojas <154765747+JosePRU24@users.noreply.github.com>
2026-07-25 12:54:00 -0600

    Increase read block size to 8MB
    Increased the read block size for PDF cracking.

--- config_bundle/Pdf-Crack.py
+++ config_bundle/Pdf-Crack.py
@@ -38,7 +38,7 @@ def try_passwords(args):
 
 def crack_pdf(pdf_file, wordlist_file):
     try:
-      read_block_size = 1024 * 1024
+      read_block_size = 8 * 1024 * 1024
       encoder = "utf-8"
       process_count = 4
       with Pool(processes=process_count) as pool:

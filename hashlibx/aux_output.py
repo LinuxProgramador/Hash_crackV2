431c2c52bd2cb18bd598cb11aa6931dcc6137e3e
JP Rojas <154765747+JosePRU24@users.noreply.github.com>
2026-07-28 23:05:25 -0600

    Check for leading/trailing whitespace in password
    Add warning for passwords with leading or trailing whitespace.

--- hashlibx/aux_output.py
+++ hashlibx/aux_output.py
@@ -17,7 +17,9 @@ def auxiliary_crack(word, wpa_psk, ssid):
    print("=" * 50)
    print(f">>> Recovered Password: {word}".center(50))
    print("=" * 50 + "\n")
-
+   if word != word.strip():
+    print("[WARNING:] The password contains leading or trailing whitespace")
+    
  except Exception as error:
    print(f"[ERROR]: {error}")
    sys.exit(1)

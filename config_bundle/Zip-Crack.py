f1eef631c3289dba61762259c875ee150addf559
JP Rojas <154765747+JosePRU24@users.noreply.github.com>
2026-07-29 18:54:59 -0600

    Implement signal handling in multiprocessing pool
    Added an init_worker function to ignore SIGINT and SIGTSTP signals in the multiprocessing pool.

--- config_bundle/Zip-Crack.py
+++ config_bundle/Zip-Crack.py
@@ -5,9 +5,12 @@ import sys
 import subprocess, signal, shutil
 from multiprocessing import Pool, cpu_count
 
+def init_worker():
+   signal.signal(signal.SIGINT, signal.SIG_IGN)
+   signal.signal(signal.SIGTSTP, signal.SIG_IGN)
+
+
 def try_passwords(args):
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
-    signal.signal(signal.SIGTSTP, signal.SIG_IGN)
     zip_file, file_7z, passwords = args
 
     for pwd in passwords:
@@ -76,7 +79,7 @@ def crack_zip(zip_file, wordlist_file):
       encoder = "utf-8"
       process_count = max(1, cpu_count() - 1)
       result = None
-      with Pool(processes=process_count) as pool:
+      with Pool(processes=process_count, initializer=init_worker) as pool:
         with open(wordlist_file, 'r', encoding=encoder, errors='ignore') as f:
             last_line = ""
             while True:

000d3758adc838f0ddb897b7e67e1b93e134aeb2
JP Rojas <154765747+JosePRU24@users.noreply.github.com>
2026-07-29 18:37:38 -0600

    Implement signal handling in multiprocessing pool
    Added init_worker function to handle signal ignoring in multiprocessing pool.

--- config_bundle/Pdf_Crack_termux.py
+++ config_bundle/Pdf_Crack_termux.py
@@ -6,10 +6,12 @@ import signal
 from multiprocessing import Pool, cpu_count
 from pypdf import PdfReader
 
+def init_worker():
+   signal.signal(signal.SIGINT, signal.SIG_IGN)
+   signal.signal(signal.SIGTSTP, signal.SIG_IGN)
+
 
 def try_passwords(args):
-  signal.signal(signal.SIGINT, signal.SIG_IGN)
-  signal.signal(signal.SIGTSTP, signal.SIG_IGN)
   pdf_file, passwords = args
   reader = PdfReader(pdf_file)
   for pwd in passwords:
@@ -41,7 +43,7 @@ def crack_pdf(pdf_file, wordlist_file):
       encoder = "utf-8"
       process_count = max(1, cpu_count() - 1)
       result = None
-      with Pool(processes=process_count) as pool:
+      with Pool(processes=process_count, initializer=init_worker) as pool:
         with open(wordlist_file, 'r', encoding=encoder, errors='ignore') as keywords_read:
             last_line = ""
             while True:

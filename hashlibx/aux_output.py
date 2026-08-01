07d4df4fef26f6054f683a24ea8ed8a76e4b0a57
JP Rojas <154765747+JosePRU24@users.noreply.github.com>
2026-08-01 16:21:56 -0600

    Refactor password output logic in auxiliary_crack

--- hashlibx/aux_output.py
+++ hashlibx/aux_output.py
@@ -4,21 +4,21 @@ import sys
 
 def auxiliary_crack(word, wpa_psk, ssid):
  try:
+  if not type(word) is bool:
+    print("\n" + "=" * 50)
 
-   print("\n" + "=" * 50)
+    if wpa_psk:
+      print("[ SSID ]".center(50))
+      print("=" * 50)
+      print(f">>> ssid: {ssid}".center(50))
 
-   if wpa_psk:
-     print("[ SSID ]".center(50))
-     print("=" * 50)
-     print(f">>> ssid: {ssid}".center(50))
-
-   print("=" * 50)
-   print("[ PASSWORD FOUND ]".center(50))
-   print("=" * 50)
-   print(f">>> Recovered Password: {word}".center(50))
-   print("=" * 50 + "\n")
-   if word != word.strip():
-    print("[WARNING:] The password contains leading or trailing whitespace")
+    print("=" * 50)
+    print("[ PASSWORD FOUND ]".center(50))
+    print("=" * 50)
+    print(f">>> Recovered Password: {word}".center(50))
+    print("=" * 50 + "\n")
+    if word != word.strip():
+      print("[WARNING:] The password contains leading or trailing whitespace")
     
  except Exception as error:
    print(f"[ERROR]: {error}")

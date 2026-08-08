17f37661c16d062030aea62721402607b7c6c933
JP Rojas <154765747+JosePRU24@users.noreply.github.com>
2026-08-08 14:34:52 -0600

    Update cpu_selector.py

--- hashlibx/cpu_selector.py
+++ hashlibx/cpu_selector.py
@@ -6,9 +6,14 @@ from multiprocessing import cpu_count
 def get_cpu_allocation(hash_type, cpu_num, use_cpu_num ):
   try:
 
-    is_limited = hash_type in ["argon2id", "scrypt" ,"yescrypt"]
+    is_limited = hash_type in {"argon2id", "scrypt" ,"yescrypt"}
     if is_limited:
-        process_count = cpu_num if use_cpu_num  else 1
+        if use_cpu_num and 1 <= cpu_num <= cpu_count():
+           process_count = cpu_num
+        elif use_cpu_num:
+           process_count = cpu_count()
+        else:
+           process_count = 1
 
     elif cpu_num and cpu_num <= cpu_count():
         process_count = cpu_num

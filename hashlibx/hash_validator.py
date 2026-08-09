58569ab5fc201fb0f8039132bd5c8d735f8b4390
JP Rojas <154765747+JosePRU24@users.noreply.github.com>
2026-08-08 23:27:10 -0600

    Add files via upload

---
+++ hashlibx/hash_validator.py
@@ -0,0 +1,706 @@
+#!/usr/bin/env python3
+
+import time
+import sys
+import binascii
+
+from gmssl import sm3, func
+from whirlpool import new as wpl
+from Crypto.Hash import RIPEMD160, MD4
+
+from hashlib import (
+    md5,
+    sha1,
+    sha224,
+    sha384,
+    sha256,
+    sha512,
+    sha3_256,
+    sha3_224,
+    sha3_384,
+    sha3_512,
+    blake2s,
+    blake2b,
+    shake_128,
+    shake_256,
+    pbkdf2_hmac,
+    algorithms_available,
+    new,
+)
+
+from hashlib import scrypt as scrypt_hashlib
+
+from passlib.hash import (
+    sha256_crypt,
+    sha512_crypt,
+    md5_crypt,
+    apr_md5_crypt,
+    msdcc2,
+    phpass,
+    scrypt,
+)
+
+from passlib.hash import pbkdf2_sha256 as pbkf_sha2_passlib
+from passlib.hash import pbkdf2_sha1 as pbkf_sha1_passlib
+from passlib.hash import pbkdf2_sha512 as pbkf_sha5_passlib
+
+from bcrypt import checkpw
+from argon2.exceptions import VerifyMismatchError
+
+
+if sys.platform == "linux":
+    from pyescrypt import WrongPassword, WrongPasswordConfiguration
+
+
+
+
+HASH_ALGORITHMS_INFO = {
+    "md5": md5,
+    "sha1": sha1,
+    "sha224": sha224,
+    "sha256": sha256,
+    "sha384": sha384,
+    "sha512": sha512,
+    "sha3-224": sha3_224,
+    "sha3-256": sha3_256,
+    "sha3-384": sha3_384,
+    "sha3-512": sha3_512,
+    "blake2b": blake2b,
+    "blake2s": blake2s,
+    "sha256crypt": sha256_crypt,
+    "sha512crypt": sha512_crypt,
+    "md5crypt": md5_crypt,
+    "apr1": apr_md5_crypt,
+    "phpass": phpass,
+}
+
+
+
+
+def mysql5_x_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    try:
+        return (
+            "*" + sha1(sha1(data).digest()).hexdigest().lower()
+            == target_hash.lower()
+        )
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def whirlpool_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    try:
+        return wpl(data).hexdigest().lower() == target_hash.lower()
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def sha256sum_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    try:
+        return (
+            sha256((word + "\n").encode(encoder)).hexdigest().lower()
+            == target_hash.lower()
+        )
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def sha512sum_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    try:
+        return (
+            sha512((word + "\n").encode(encoder)).hexdigest().lower()
+            == target_hash.lower()
+        )
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def sm3_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    try:
+        if "sm3" in algorithms_available:
+            h = new("sm3")
+            h.update(data)
+            generated_hash = h.hexdigest()
+        else:
+            generated_hash = sm3.sm3_hash(
+                func.bytes_to_list(data)
+            )
+
+        return generated_hash.lower() == target_hash.lower()
+
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def ntlm_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    try:
+        h = MD4.new()
+        h.update(word.encode("utf-16le"))
+
+        return h.hexdigest().lower() == target_hash.lower()
+
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def sha512_256_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    try:
+        return (
+            new("sha512_256", data).hexdigest().lower()
+            == target_hash.lower()
+        )
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def ssha_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+  try:
+
+    digest = precomputed["digest"]
+
+    h = sha1(data)
+    h.update(precomputed["salt"])
+
+    return h.digest() == digest
+
+  except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+def shake_256_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+
+  try:
+    generated_hash = shake_256(data).hexdigest(
+        len(target_hash) // 2
+    )
+
+    return generated_hash.lower() == target_hash.lower()
+
+  except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def shake_128_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+
+  try:
+
+    s = shake_128()
+    s.update(data)
+
+    generated_hash = s.digest(
+        len(bytes.fromhex(target_hash))
+    ).hex()
+
+    return generated_hash.lower() == target_hash.lower()
+
+  except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def ripemd_160_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+
+  try:
+    if "ripemd160" in algorithms_available:
+        generated_hash = new(
+            "ripemd160",
+            data
+        ).hexdigest()
+
+    else:
+        h = RIPEMD160.new()
+        h.update(data)
+        generated_hash = h.hexdigest()
+
+    return generated_hash.lower() == target_hash.lower()
+
+  except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def bcrypt_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+
+  try:
+
+    if wait_time == "y":
+        time.sleep(0.20)
+
+    return checkpw(
+        data,
+        target_hash.encode(encoder)
+    )
+
+  except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def dcc2_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+
+    time.sleep(0.02)
+
+    if wait_time == "y":
+        time.sleep(0.20)
+
+    try:
+        if target_hash.startswith("$DCC2$"):
+            temp = target_hash.split("#")
+
+            return msdcc2.verify(
+                word,
+                temp[2],
+                user=temp[1]
+            )
+
+        else:
+            return msdcc2.verify(
+                word,
+                target_hash,
+                user=user
+            )
+
+    except ValueError as value_error:
+        print(
+            f"[!] Error verifying DCC2 hash: {value_error}. "
+            "Please ensure the hash format and username are correct"
+        )
+        return ["_error_"]
+
+
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+
+def wpa_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+
+  try:
+
+    if 8 <= len(word) <= 63:
+        derived_key = pbkdf2_hmac(
+            "sha1",
+            data,
+            ssid.encode(encoder),
+            4096,
+            32
+        )
+
+        return (
+            derived_key.hex().lower()
+            == target_hash.lower()
+        )
+
+    return False
+
+  except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+
+def pbkdf2_sha256_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    if wait_time == "y":
+        time.sleep(0.20)
+
+    try:
+        if precomputed is not None:
+            key = pbkdf2_hmac(
+                "sha256",
+                data,
+                precomputed["salt"],
+                precomputed["iterations"],
+                precomputed["dklen"]
+            )
+
+            return key == precomputed["key"]
+
+        else:
+            return pbkf_sha2_passlib.verify(
+                word,
+                target_hash
+            )
+
+    except (
+        binascii.Error,
+        ValueError,
+        TypeError,
+        IndexError,
+        KeyError
+    ):
+        return pbkf_sha2_passlib.verify(
+            word,
+            target_hash
+        )
+
+
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+
+def pbkdf2_sha1_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    if wait_time == "y":
+        time.sleep(0.20)
+
+    try:
+        if precomputed is not None:
+            key = pbkdf2_hmac(
+                "sha1",
+                data,
+                precomputed["salt"],
+                precomputed["iterations"],
+                precomputed["dklen"]
+            )
+
+            return key == precomputed["key"]
+
+        else:
+            return pbkf_sha1_passlib.verify(
+                word,
+                target_hash
+            )
+
+    except (
+        binascii.Error,
+        ValueError,
+        TypeError,
+        IndexError,
+        KeyError
+    ):
+        return pbkf_sha1_passlib.verify(
+            word,
+            target_hash
+        )
+
+
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+
+def pbkdf2_sha512_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    if wait_time == "y":
+        time.sleep(0.20)
+
+    try:
+        if precomputed is not None:
+            key = pbkdf2_hmac(
+                "sha512",
+                data,
+                precomputed["salt"],
+                precomputed["iterations"],
+                precomputed["dklen"]
+            )
+
+            return key == precomputed["key"]
+
+        else:
+            return pbkf_sha5_passlib.verify(
+                word,
+                target_hash
+            )
+
+    except (
+        binascii.Error,
+        ValueError,
+        TypeError,
+        IndexError,
+        KeyError
+    ):
+        return pbkf_sha5_passlib.verify(
+            word,
+            target_hash
+        )
+
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def argon2id_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    if wait_time == "y":
+        time.sleep(0.20)
+
+    try:
+        ph.verify(
+            target_hash,
+            word
+        )
+
+        return True
+
+    except VerifyMismatchError:
+        return False
+
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+def scrypt_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    if wait_time == "y":
+        time.sleep(0.20)
+
+    try:
+        if precomputed is not None:
+            derived_key = scrypt_hashlib(
+                data,
+                salt=precomputed["salt"],
+                n=precomputed["n"],
+                r=precomputed["r"],
+                p=precomputed["p"],
+                dklen=precomputed["dklen"]
+            )
+
+            return derived_key == precomputed["key"]
+
+        else:
+            return scrypt.verify(
+                word,
+                target_hash
+            )
+
+    except (
+        binascii.Error,
+        ValueError,
+        TypeError,
+        IndexError,
+        KeyError
+    ):
+        return scrypt.verify(
+            word,
+            target_hash
+        )
+
+
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+
+def yescrypt_hash(
+    word, data, target_hash, hash_type, encoder,
+    wpa_psk, ssid, user, wait_time, ph,
+    yescrypt_hash, context, precomputed, hashes_dic
+):
+    if wait_time == "y":
+        time.sleep(0.20)
+
+    try:
+        yescrypt_hash.compare(
+            data,
+            target_hash.encode(encoder)
+        )
+
+        return True
+
+    except (
+        WrongPassword,
+        WrongPasswordConfiguration
+    ):
+        return False
+
+    except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]
+
+
+
+
+
+
+
+def validate_word(
+    word,
+    data,
+    target_hash,
+    hash_type,
+    encoder,
+    wpa_psk,
+    ssid,
+    user,
+    wait_time,
+    ph,
+    yescrypt_hash,
+    context,
+    precomputed,
+    hashes_dic
+):
+
+  try:
+
+    if hash_type in hashes_dic:
+        return hashes_dic[hash_type](
+            word,
+            data,
+            target_hash,
+            hash_type,
+            encoder,
+            wpa_psk,
+            ssid,
+            user,
+            wait_time,
+            ph,
+            yescrypt_hash,
+            context,
+            precomputed
+        )
+
+
+
+    if hash_type in HASH_ALGORITHMS_INFO:
+
+        if hash_type == "sha512crypt":
+            time.sleep(0.02)
+
+    if wait_time == "y" and hash_type in {
+        "sha256crypt",
+        "sha512crypt",
+        "md5crypt",
+        "apr1",
+        "phpass"
+    }:
+        time.sleep(0.20)
+
+    if hash_type in {
+        "sha256crypt",
+        "sha512crypt",
+        "md5crypt",
+        "apr1",
+        "phpass"
+    }:
+
+        if hash_type in {
+            "sha256crypt",
+            "sha512crypt",
+            "md5crypt",
+            "apr1"
+        }:
+
+            try:
+                return context.verify(
+                    word,
+                    target_hash
+                )
+
+            except Exception:
+                return HASH_ALGORITHMS_INFO[
+                    hash_type
+                ].verify(
+                    word,
+                    target_hash
+                )
+
+        return HASH_ALGORITHMS_INFO[
+            hash_type
+        ].verify(
+            word,
+            target_hash
+        )
+
+
+
+    if hash_type in HASH_ALGORITHMS_INFO:
+
+        generated_hash = HASH_ALGORITHMS_INFO[
+            hash_type
+        ](data).hexdigest()
+
+        return (
+            generated_hash.lower()
+            == target_hash.lower()
+        )
+
+    return False
+
+  except Exception as error:
+        print(f"[ERROR]: {error}")
+        return ["_error_"]

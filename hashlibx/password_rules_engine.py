cbb4616ed13b093f472b606ca32b3ba2ebf6becc
JP Rojas <154765747+JosePRU24@users.noreply.github.com>
2026-08-11 15:04:07 -0600

    Update password_rules_engine.py

--- hashlibx/password_rules_engine.py
+++ hashlibx/password_rules_engine.py
@@ -2,143 +2,162 @@
 
 from itertools import product
 
-def rules_parameters(word, rules, numbers, symbols, vocals, digits, translation_table, valid_rules):
-    
-    if rules and len(rules) == 1:
-          rule = rules[0]
-    elif rules and len(rules) >= 2:
-        rule = "".join(rules[:2])
+
+RULE_SYMBOL_TRANSLATIONS = {'64', '46'}
+RULE_NUMBER_TRANSLATIONS = {'61', '16'}
+RULE_CAPITAL_TRANSLATIONS = {'56', '65'}
+RULE_UPPER_TRANSLATIONS = {'26', '62'}
+RULE_LOWER_TRANSLATIONS = {'36', '63'}
+
+RULE_UPPER_NUMBERS = {'12', '21'}
+RULE_LOWER_NUMBERS = {'13', '31'}
+RULE_CAPITAL_NUMBERS = {'15', '51'}
+
+RULE_UPPER_SYMBOLS = {'42', '24'}
+RULE_LOWER_SYMBOLS = {'34', '43'}
+RULE_CAPITAL_SYMBOLS = {'54', '45'}
 
 
-    chosen_rules = rule if rule in valid_rules else ''
-    if not chosen_rules:
-       yield word
-       return
-        
-    if chosen_rules:
-        if chosen_rules == '1':
-           for number in numbers:
-              yield word + number
+def rules_parameters(
+    word,
+    rules,
+    numbers,
+    symbols,
+    vocals,
+    digits,
+    translation_table,
+    valid_rules
+):
+
+    if not rules:
+        yield word
+        return
+
+    rule = rules[0] if len(rules) == 1 else ''.join(rules[:2])
 
 
-        elif chosen_rules == '4':
-           for symbol in symbols:
-              yield word + symbol
+    if rule not in valid_rules:
+        yield word
+        return
 
+    chosen_rules = rule
 
-        elif chosen_rules == '3':
-              yield word.lower()
+    if chosen_rules == '1':
+        for number in numbers:
+            yield word + number
 
-        elif chosen_rules == '2':
-              yield word.upper()
+    elif chosen_rules == '4':
+        for symbol in symbols:
+            yield word + symbol
 
-        elif chosen_rules == '5':
-              yield word.capitalize()
+    elif chosen_rules == '3':
+        yield word.lower()
 
-        elif chosen_rules == '6':
-            yield word.translate(translation_table)
+    elif chosen_rules == '2':
+        yield word.upper()
 
-        elif chosen_rules == '7':
-              word = word.capitalize()
-              for number in numbers:
-                 word_number = word + number
-                 for symbol in symbols:
-                    yield word_number + symbol
+    elif chosen_rules == '5':
+        yield word.capitalize()
 
+    elif chosen_rules == '6':
+        yield word.translate(translation_table)
 
-        elif chosen_rules == '8':
-              yield word[::-1]
+    elif chosen_rules == '7':
+        word = word.capitalize()
 
+        for number in numbers:
+            word_number = word + number
 
-        elif chosen_rules == '9':
-              yield word + word
-
-
-        elif chosen_rules == '10':
-             temp_word = word
-             for vocal in vocals:
-                 temp_word = temp_word.replace(vocal, '')
-                 temp_word = temp_word.replace(vocal.upper(), '')
-             yield temp_word
-
-
-        elif chosen_rules == '11':
-             for number in numbers:
-                 yield number + word
-
-        elif chosen_rules == '00':
-             for symbol in symbols:
-                 yield symbol + word
-
-
-        elif chosen_rules == 'HBA':
-            for r in range(1, 5):
-               for combo in product(digits, repeat=r):
-                  yield word + "".join(combo)
-
-
-        elif chosen_rules in {'64','46'}:
-            word = word.translate(translation_table)
             for symbol in symbols:
-               yield  word + symbol
+                yield word_number + symbol
 
+    elif chosen_rules == '8':
+        yield word[::-1]
 
-        elif chosen_rules in {'61','16'}:
-               word = word.translate(translation_table)
-               for number in numbers:
-                  yield word + number
+    elif chosen_rules == '9':
+        yield word + word
 
+    elif chosen_rules == '10':
+        temp_word = word
 
-        elif chosen_rules in {'56','65'}:
-               word = word.translate(translation_table)
-               yield word.capitalize()
+        for vocal in vocals:
+            temp_word = temp_word.replace(vocal, '')
+            temp_word = temp_word.replace(vocal.upper(), '')
 
-        elif chosen_rules in {'26','62'}:
-              word = word.translate(translation_table)
-              yield word.upper()
+        yield temp_word
 
+    elif chosen_rules == '11':
+        for number in numbers:
+            yield number + word
 
-        elif chosen_rules in {'36','63'}:
-              word = word.translate(translation_table)
-              yield word.lower()
+    elif chosen_rules == '00':
+        for symbol in symbols:
+            yield symbol + word
 
+    elif chosen_rules == 'HBA':
+        for r in range(1, 5):
+            for combo in product(digits, repeat=r):
+                yield word + ''.join(combo)
 
-        elif chosen_rules in {'12','21'}:
-              word = word.upper()
-              for number in numbers:
-                 yield word + number
+    elif chosen_rules in RULE_SYMBOL_TRANSLATIONS:
+        word = word.translate(translation_table)
 
+        for symbol in symbols:
+            yield word + symbol
 
-        elif chosen_rules in {'13','31'}:
-              word = word.lower()
-              for number in numbers:
-                 yield word + number
+    elif chosen_rules in RULE_NUMBER_TRANSLATIONS:
+        word = word.translate(translation_table)
 
+        for number in numbers:
+            yield word + number
 
-        elif chosen_rules in {'15','51'}:
-              word = word.capitalize()
-              for number in numbers:
-                 yield word + number
+    elif chosen_rules in RULE_CAPITAL_TRANSLATIONS:
+        word = word.translate(translation_table)
+        yield word.capitalize()
 
+    elif chosen_rules in RULE_UPPER_TRANSLATIONS:
+        word = word.translate(translation_table)
+        yield word.upper()
 
-        elif chosen_rules in {'42','24'}:
-              word = word.upper()
-              for symbol in symbols:
-                 yield word + symbol
+    elif chosen_rules in RULE_LOWER_TRANSLATIONS:
+        word = word.translate(translation_table)
+        yield word.lower()
 
+    elif chosen_rules in RULE_UPPER_NUMBERS:
+        word = word.upper()
 
-        elif chosen_rules in {'34','43'}:
-              word = word.lower()
-              for symbol in symbols:
-                 yield word + symbol
+        for number in numbers:
+            yield word + number
 
+    elif chosen_rules in RULE_LOWER_NUMBERS:
+        word = word.lower()
 
-        elif chosen_rules in {'54','45'}:
-              word = word.capitalize()
-              for symbol in symbols:
-                 yield word + symbol
+        for number in numbers:
+            yield word + number
 
+    elif chosen_rules in RULE_CAPITAL_NUMBERS:
+        word = word.capitalize()
 
-        elif chosen_rules == 'lcu':
-            word = word[:1].lower() + word[1:].upper()
-            yield word
+        for number in numbers:
+            yield word + number
+
+    elif chosen_rules in RULE_UPPER_SYMBOLS:
+        word = word.upper()
+
+        for symbol in symbols:
+            yield word + symbol
+
+    elif chosen_rules in RULE_LOWER_SYMBOLS:
+        word = word.lower()
+
+        for symbol in symbols:
+            yield word + symbol
+
+    elif chosen_rules in RULE_CAPITAL_SYMBOLS:
+        word = word.capitalize()
+
+        for symbol in symbols:
+            yield word + symbol
+
+    elif chosen_rules == 'lcu':
+        yield word[:1].lower() + word[1:].upper()
